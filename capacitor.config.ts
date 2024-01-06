742b5a54d79b351064c5f502796c3cd6e51a7127
nataliareyess <148658946+nataliareyess@users.noreply.github.com>
2024-01-06 19:24:31 -0300

    archivos

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph2.u2.gonzalovillagra',
+  appName: 'ph2-u2-gonzalovillagra',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
